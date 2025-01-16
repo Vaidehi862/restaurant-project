eab594fb3c89f84774593be269d0fdf29435304b
Vaidehi862 <vaidehibagale@gmail.com>
2025-01-16 09:15:26 +0530

    Add files via upload

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,151 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import rating_starts from './rating_starts.png'
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Deserts",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Pure Veg",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    ]
+
+export const food_list = [
+    {
+        food_id: 1,
+        food_name: "Greek salad",
+        food_image: food_1,
+        food_price: 222,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Salad"
+    },
+    {
+        food_id: 2,
+        food_name: "Sandwich",
+        food_image: food_2,
+        food_price: 110,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Sandwich"
+    }, {
+        food_id: 3,
+        food_name: "Cake",
+        food_image: food_3,
+        food_price: 337,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Cake"
+    }, {
+        food_id: 4,
+        food_name: "Special Salad",
+        food_image: food_4,
+        food_price: 325,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Salad"
+    }, {
+        food_id: 5,
+        food_name: "Lasagna Rolls",
+        food_image: food_5,
+        food_price: 205,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Rolls"
+    }, {
+        food_id: 6,
+        food_name: "Pure Veg",
+        food_image: food_6,
+        food_price: 400,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Pure Veg"
+    }, {
+        food_id: 7,
+        food_name: "Red sauce pasta",
+        food_image: food_7,
+        food_price: 330,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Pasta"
+    }, {
+        food_id: 8,
+        food_name: "White sauce pasta",
+        food_image: food_8,
+        food_price: 290,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Pasta"
+    }, {
+        food_id: 9,
+        food_name: "Ripple Ice Cream",
+        food_image: food_9,
+        food_price: 225,
+        food_desc: "Food provides essential nutrients for overall health and well-being",
+        food_category: "Deserts"
+    }, 
+]
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>your favourite food here</h2>
+                <p>Welcome to our restaurant, where great food and warm hospitality meet. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
